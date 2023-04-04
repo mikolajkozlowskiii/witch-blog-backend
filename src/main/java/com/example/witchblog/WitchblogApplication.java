81eaabc053cf7ad178a7bc5f2687d9edf375eded
mikolajkozlowskiii <mikolajkozlowskiii@gmail.com>
2023-04-04 10:00:57 +0200

    Initial commit

---
+++ src/main/java/com/example/witchblog/WitchblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.witchblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitchblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WitchblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/witchblog/WitchblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.witchblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitchblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
